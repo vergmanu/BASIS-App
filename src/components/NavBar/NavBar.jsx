07a9ba13c171ab1a6e8942c77857e5bf5ea594fc
vergmanu <vergmanu@gmail.com>
2022-05-03 15:19:57 -0700

    MERN-Stack Infrastructure

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+// Using the import below, we can call any exported function using: userService.someMethod()
+import * as userService from '../../utilities/users-service';
+
+
+export default function NavBar({ user, setUser }) {
+  // Add the following function
+  function handleLogOut() {
+    // Delegate to the users-service
+    userService.logOut(setUser());
+    // Update state will also cause a re-render
+    setUser(null);
+  }
+  return (
+    <nav>
+      <Link to="/orders">Order History</Link>
+      &nbsp; | &nbsp;
+      <Link to="/orders/new">New Order</Link>
+      &nbsp;&nbsp;<span>Welcome, {user.name}</span>
+      &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
+    </nav>
+  );
+}
